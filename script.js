0026402a3453c4373e959812d7875f5d105c8ce7
Eric Liu <ericliu314@gmail.com>
2018-03-22 23:18:11 -0400

    Create homepage, modal, markdown editor/live preview in mdbootstrap

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(function() {
     
+    const TAB_KEY = 9;
+    const ENTER_KEY = 13;
+
+    // Autosave editor on page load:
+    let lastAutosaveTime = moment();
+    autosaveEditor(true);
+    updateFileSize(0);
+
     // Initialize Marked options:
     marked.setOptions({
         renderer: new marked.Renderer(),
@@ -17,16 +25,68 @@ $(function() {
         $("#markdown-editor").focus();
     });
 
-    $("#markdown-editor").on("keyup", function(e) {
+    $("#markdown-editor").on("keydown", function(e) {
+        if (e.keyCode == TAB_KEY) {
+            document.execCommand("insertHTML", false, "&#9");
+            e.preventDefault();
+        }
+
         setTimeout(function() {
             updatePreview();
         }, 0);
+
+        setTimeout(function() {
+            autosaveEditor();
+        }, 0);
     });
+
+    $("#modalLoginAvatar").on('hidden.bs.modal', function() {
+        // Reset the modal input:
+        $("#input-new-note").val("");
+    });
+
+    $("#search-bar").on("keyup", function(e) {
+        const searchQuery = $(this).val();
+
+        if (e.keyCode == ENTER_KEY) {
+            e.preventDefault();
+            updateSearchArea(searchQuery);
+        }
+
+        // Reset the dynamic search area:
+        if (!searchQuery) {
+            resetSearchArea();
+        }
+    });
+
+    function autosaveEditor(override) {
+        const currentTime = moment();
+        
+        if (!override && (currentTime - lastAutosaveTime < 10000)) {
+            return;
+        }
+        
+        lastAutosaveTime = currentTime;
+
+        const markdownRawText = $("#markdown-editor").text();
+
+        $("#autosave-datetime").text(moment(lastAutosaveTime).format('MM/DD/YYYY h:mm:ss a'));
+    }
 
     function updatePreview() {
         const markdownRawText = document.getElementById("markdown-editor").innerText;
         const markdownHTML = marked(markdownRawText);
         MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
         $("#preview").html(markdownHTML);
+    }
+
+    function updateSearchArea(searchQuery) {
+        // test
+        console.log(searchQuery);
+    }
+
+    function resetSearchArea() {
+        // test
+        console.log('reset');
     }
 });
